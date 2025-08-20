6054b0b4b856b3bc8ef92745e273928cf6088a94
Nathan Anjos <nathabe30@gmail.com>
2025-08-19 21:13:34 -0300

    Terceiro commit

---
+++ Homeworks/exe12.js
@@ -0,0 +1,25 @@
+import readlineSync from 'readline-sync';
+
+let salarioanual = readlineSync.question ("Qual o salário anual ? ")
+let tempos = readlineSync.question ("Qual o tempo de serviço ? ")
+let desempenho = readlineSync.question ("Digite o desempenho : ")
+
+let bonus = 0;
+
+if (tempos <= 2 ) {
+   bonus = salarioanual * 0.05 
+}else if (tempos >=3 && tempos <= 5 ){
+    bonus = salarioanual * 0.10
+}else {
+    bonus = salarioanual * 0.15
+}
+
+if (desempenho >= 1 && desempenho <= 4){
+    console.log("não obteve bônus por desempenho")
+} else if (desempenho >= 5 && desempenho <= 7 ){
+    bonus = salarioanual * 0.20
+} else {
+    bonus = salarioanual * 0.50
+}
+
+console.log (`O bônus total foi de ${bonus}`)
