8e857971bf5f8641e7ede9db291238a81c000dca
Nathan Anjos <nathabe30@gmail.com>
2025-08-29 18:32:53 -0300

    Nin9 Commit

---
+++ Desafios/desafio3.js
@@ -0,0 +1,7 @@
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+function resultados(item, indice){
+    if (numeros[indice] % 2 == 0){
+        console.log (`Esse número ${numeros[indice]} é par`)
+    }
+} numeros.forEach(resultados)
